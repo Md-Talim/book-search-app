dc35dd7245291faad8a87b007b484b1e2975e6d4
Mohammad Talim <talimmohammad116@gmail.com>
2024-06-09 20:09:45 +0530

    create BookCard component to display book details

---
+++ src/components/BookCard.js
@@ -0,0 +1,11 @@
+const BookCard = ({ book, addToBookshelf }) => {
+  return (
+    <div className="book-card">
+      <h3>{book.title}</h3>
+      <p>{book.author_name ? book.author_name.join(", ") : "Unknown Author"}</p>
+      <button onClick={() => addToBookshelf(book)}>Add to Bookshelf</button>
+    </div>
+  );
+};
+
+export default BookCard;
